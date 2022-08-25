eac102052832aab239f151ba5d690c5ef1d4e064
Jose Antonio Trindade Borges da Costa <jatborgesdacosta@gmail.com>
2022-08-25 13:21:22 -0300

    segundo commit de pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "jatborgesdacosta@gmail.com"
 let profissao = "Fisico"
 let idade = 64
 let viagem = "Barcelona"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagem)
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagem}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
